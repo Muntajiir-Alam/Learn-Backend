928bae61cb0e1afbf71fa51fac075eeab25d73e2
Muntajir Alam <muntajiralam20074@gmail.com>
2026-08-21 19:57:54 +0530

    feat: add about and contact routes to server

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/about", (req, res) => {
+    res.send("This is the about page.");
+});
+
+app.get("/contact", (req, res)=>{
+    res.send("this is contact us page")
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
